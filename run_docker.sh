d1f9012f74ccd4ff5217cb0a3d76e1661d3386f6
yara-ayman <Yaraayman176@gmail.com>
2022-11-03 02:10:55 +0200

    change ecr

--- run_docker.sh
+++ run_docker.sh
@@ -3,9 +3,10 @@
 ## Complete the following steps to get Docker running locally
 ## This file was used to create the Original App image
 
-# Step 1:
+# Step 1:latest
 # Build image and add a descriptive tag
-docker build -t web-app:1.0 .
+#docker build -t web-app:1.0 .
+docker build -t latest .
 
 # Step 2:
 # List docker images
@@ -13,4 +14,5 @@ docker image ls
 
 # Step 3:
 # Run flask app
-docker run -p 8000:5000 web-app:1.0
+#docker run -p 8000:5000 web-app:1.0
+docker run -p 8000:5000 latest
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,16 +6,19 @@
 
 # Step 1:
 # Authenticate
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273801816274.dkr.ecr.us-east-1.amazonaws.com
-
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273801816274.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 564374043688.dkr.ecr.us-east-1.amazonaws.com
 # Step 2:
 # Build image
-docker build -t web-app:1.0 .
+#docker build -t web-app:1.0 .
+docker build -t ecr .
 
 # Step 2:  
 # Tag the image to push it to the repo
-docker tag web-app:1.0 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+#docker tag web-app:1.0 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker tag ecr:latest 564374043688.dkr.ecr.us-east-1.amazonaws.com/ecr:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+#docker push 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker push 564374043688.dkr.ecr.us-east-1.amazonaws.com/ecr:latest
